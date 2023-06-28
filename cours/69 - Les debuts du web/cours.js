bab356d496060f46a2749b2adff1c55574452529
Jérémy Mouzin <jeremy@javascriptdezero.com>
2023-06-28 16:05:11 +0200

    Leçon sur les débuts du web

---
+++ cours/69 - Les debuts du web/cours.js
@@ -0,0 +1,23 @@
+/* Les débuts du web 
+
+- Liens hypertextes entre les pages web
+
+Inconvénients :
+- Lent car on recharge toutes les données de chaque page à chaque fois
+- Toute la page disparaît avant de réapparaître
+- Pas aussi agréable que les "applications de bureau" (celles qui s'exécute directement sur votre ordinateur et pas dans un navigateur)
+
+En mars 1999 :
+- XMLHttpRequest (XHR) (AJAX : Asynchronous JavaScript and XML)
+  - Permet d'envoyer une requête au serveur et de recevoir des données SANS recharger la page complète !
+  - On peut ainsi mettre à jour uniquement une partie de la page web grâce au langage JavaScript + DOM
+
+Entre 2002 et 2004 :
+- Implémentation d'XMLHttpRequest dans les navigateurs de l'époque
+
+Applications utilisant les XHR : Gmail (2004), Google Maps (2005)
+
+En 2006 :
+- Le W3C (World Wide Web Consortium) publie une spécification officielle pour XMLHttpRequest qui devient un standard web
+*/
+
