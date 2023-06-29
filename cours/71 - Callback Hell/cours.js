1b721c4e75afa05a66359a9f4f2d5fbfcc45cc99
Jérémy Mouzin <jeremy@javascriptdezero.com>
2023-06-29 11:10:08 +0200

    Leçon Callback Hell

---
+++ cours/71 - Callback Hell/cours.js
@@ -0,0 +1,8 @@
+/* Callback Hell :
+
+- Difficile de lire le code (pyramide du malheur, 🇬🇧 pyramid of doom)
+- Ne fonctionne pas de façon séquentielle (recette de cuisine), tout est asynchrone et on doit "sauter" dans le code pour comprendre ce qui se passe
+
+Solution : les promesses (🇬🇧 promises)
+
+*/
---
+++ cours/71 - Callback Hell/script.js
@@ -0,0 +1,66 @@
+/* Callback Hell (L'enfer des callbacks) */
+
+let bouton = document.querySelector('#avecXHR');
+let username = document.querySelector('#username');
+let donnees = document.querySelector('#donnees');
+
+bouton.addEventListener('click', function(e) {
+  // Ne pas envoyer le formulaire
+  e.preventDefault();
+  let githubUsername = username.value;
+  envoyerRequete(githubUsername);
+});
+
+function envoyerRequete(githubUsername) {
+  let xhr = new XMLHttpRequest();
+
+  // 🚨 Faille de sécurité possible ici
+  xhr.open("get", `https://api.github.com/users/${githubUsername}`, true);
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200 || xhr.status === 304) {
+        donnees.textContent = xhr.responseText;
+
+        let reponse = JSON.parse(xhr.responseText);
+
+        /* Envoi d'une nouvelle requête pour récupérer les dépôts */
+        let xhrDepots = new XMLHttpRequest();
+        xhrDepots.open("get", reponse.repos_url, true);
+
+        xhrDepots.onreadystatechange = function() {
+          if (xhrDepots.readyState === 4) {
+            if (xhrDepots.status === 200 || xhrDepots.status === 304) {
+              let listeDepots = JSON.parse(xhrDepots.responseText);
+              let premierDepot = listeDepots[0];
+              
+              /* Envoi d'une nouvelle requête pour récupérer les forks */
+              let xhrForks = new XMLHttpRequest();
+              xhrForks.open("get", premierDepot.forks_url, true);
+
+              xhrForks.onreadystatechange = function() {
+                if (xhrForks.readyState === 4) {
+                  if (xhrForks.status === 200 || xhrForks.status === 304) {
+                    let listeForks = JSON.parse(xhrForks.responseText);
+                    let premierFork = listeForks[0];
+                    // On se rend sur le premier fork du premier dépôt
+                    window.location = premierFork.html_url;
+                  } else {
+                    console.log("Impossible de récupérer les forks");
+                  }
+                }
+              }
+              xhrForks.send(null);
+            } else {
+              console.log("Impossible de récupérer les dépôts");
+            }
+          }
+        }
+        xhrDepots.send(null);
+      } else {
+        console.log("Une erreur s'est produite");
+      }
+    }
+  }
+  xhr.send(null);
+}
