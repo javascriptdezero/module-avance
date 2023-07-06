a62c034cdd9d2ad03f0d52f12e9b05b6efd88834
Jérémy Mouzin <jeremy@javascriptdezero.com>
2023-07-06 11:42:41 +0200

    Mot de la fin

---
+++ cours/77 - Le mot de la fin/cours.js
@@ -0,0 +1,19 @@
+/* C'est fini ! 🎉
+
+- Vous savez maintenant écrire du JavaScript moderne 👍
+- Vous devriez être autonome sur la lecture de la documentation
+- Vous savez lire, comprendre et remanier du code legacy
+- Introduction aux classes (pour amorcer l'apprentissage de la POO)
+- Introduction aux API
+  - https://github.com/public-api-lists/public-api-lists
+  - https://publicapi.dev/
+
+Et maintenant ?
+- Entraînez vos nouvelles compétences sur des exercices d'algorithmique pour vous préparer aux entretiens techniques
+- Complétez vos connaissances grâce à la documentation MDN
+- Créez un portfolio avec du JavaScript moderne
+- Apprenez le framework de votre choix, vous avez toutes les compétences fondamentales pour y arriver
+- N'hésitez pas à revenir de temps en temps sur la formation si besoin !
+- Posez-moi vos questions par email, je reste joignable pour vous aider 😉
+
+*/
